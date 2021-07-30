b2d7ef7f4ba4ab0a895bb391d53a760d911e768e
mlanca-c <mlanca-c@student.42lisboa.com>
2021-07-30 13:57:22 +0100

    Update sources Updated fractol.c file. It contains main() function and now is fully equiped to recongnize command line arguments, to display an error message in case of error, and to call the specific functions depending on the user's request.

--- sources/fractol.c
+++ sources/fractol.c
@@ -6,18 +6,81 @@
 /*   By: mlanca-c <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/29 13:10:57 by mlanca-c          #+#    #+#             */
-/*   Updated: 2021/07/29 16:16:48 by mlanca-c         ###   ########.fr       */
+/*   Updated: 2021/07/30 13:36:15 by mlanca-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
+/*
+ * When called this function displays an error message and exits the program.
+*/
+void	error_message(void)
+{
+	printf("fractol: invalid arguments.\n");
+	printf("correct format: ");
+	printf("[./fractol <fractal> [x] [y] [-p <precision>] [-c <color>]\n");
+	exit(0);
+}
+
+/*
+*/
+int	get_input_fractal(char **argv, int argc, t_input *input)
+{
+	int	i;
+
+	if (!ft_strcmp(argv[1], "Mandelbrot"))
+	{
+		input->fractal = argv[1];
+		input->c_real = 0;
+		input->c_imaginary = 0;
+		i = 2;
+	}
+	else if (!ft_strcmp(argv[1], "Julia") && argc > 3
+		&& ft_str_isnumeric(argv[2]) && ft_str_isnumeric(argv[3]))
+	{
+		input->fractal = argv[1];
+		input->c_real = ft_atoi(argv[2]);
+		input->c_imaginary = ft_atoi(argv[3]);
+		i = 4;
+	}
+	else
+		error_message();
+	return (i);
+}
+
+/*
+*/
+t_input	*init_input(char **argv, int argc, t_input *input)
+{
+	int		i;
+
+	i = get_input_fractal(argv, argc, input);
+	input->precision = 255;
+	input->color = 0x0000FF00;
+	while (i < argc)
+	{
+		if (!ft_strcmp(argv[i], "-p") && argc > i + 1
+			&& ft_str_isnumeric(argv[++i]))
+			input->precision = ft_atoi(argv[i]);
+		else if (!ft_strcmp(argv[i], "-c") && argc > i + 1
+			&& ft_str_isnumeric(argv[++i]))
+			input->color = ft_atoi(argv[i]);
+		else
+			error_message();
+		i++;
+	}
+	return (input);
+}
+
+/*
+*/
 void	get_fractal(t_data *data)
 {
-	if (ft_strcmp(data->fractal, "Mandelbrot"))
+	if (!ft_strcmp(data->input->fractal, "Mandelbrot"))
 		mandelbrot_set(data);
-	else
-		printf("Wrong Fractal\n");
+	//else if (!ft_strcmp(data->input->fractal, "Julia"))
+	//	julia_set();
 }
 
 /*
@@ -25,14 +88,16 @@ void	get_fractal(t_data *data)
 int	main(int argc, char **argv)
 {
 	t_data		data;
+	t_input		input;
 	t_img		img;
 
-	if (argc < 2)
-		return (1);
+	if (argc < 2
+		|| (ft_strcmp(argv[1], "Mandelbrot") && ft_strcmp(argv[1], "Julia")))
+		error_message();
+	data.input = init_input(argv, argc, &input);
 	data.mlx = mlx_init();
 	data.max_x = 1920;
 	data.max_y = 1080;
-	data.precision = 255;
 	data.win = mlx_new_window(data.mlx, data.max_x, data.max_y, argv[1]);
 	img.img = mlx_new_image(data.mlx, data.max_x, data.max_y);
 	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.line_len, &img.endian);
